d16b8caf114b0a5b94732385c8dc6bd47fe51b70
c2VudGljMG1tLWRldg <sentimoritz@gmail.com>
2026-06-29 20:25:16 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 
 const CACHE='slots-v1';
 self.addEventListener('install',e=>{
-e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json','./sw.js'])));
+e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./index.html','./sw.js'])));
 });
 self.addEventListener('fetch',e=>{
 e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
